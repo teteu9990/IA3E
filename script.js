aba8d8fff509fe3762c99162f0b38c126da1e02a
teteu9990 <146957642+teteu9990@users.noreply.github.com>
2024-08-02 09:28:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPergunta = document.querySelector("caixa-perguntal");
+const caixaResultado = document.querySelector("caixa-resultado");
+const caixaAlternativa = document.querySelector("caixa-alternativa");
+
+const perguntas = [//abre a lista de objetos (itens)
+    {//abre o iten
+        enunciado: "pergunta 1",
+        alternativa: ["alternativa 1",
+            "alternativa 2"
+        ]
+    }
+
+]
+
