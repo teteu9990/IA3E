6669cc3c0ac502e1b1fcd209de7ff81470a05803
teteu9990 <146957642+teteu9990@users.noreply.github.com>
2024-09-19 07:45:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-const caixaPrincipal = document.querySelector("caixa-principal");
-const caixaPergunta = document.querySelector("caixa-perguntal");
-const caixaResultado = document.querySelector("caixa-resultado");
-const caixaAlternativa = document.querySelector("caixa-alternativa");
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [//abre a lista de objetos (itens)
-    {//abre o iten
-        enunciado: "pergunta 1",
-        alternativa: ["alternativa 1",
-            "alternativa 2"
-        ]
+    {//abre o item
+        enunciado:"Você gosta da Inteligência Artificial?",
+        alternativas:["Sim","Não"]
+    },
+    {
+        enunciado:"Pergunta 2",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado:"Pergunta 3",
+        alternativas:["Alternativa 1","Alternativa 2"]
     }
-
 ]
+let posicao = 0;
+let perguntaAtual;
 
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativa){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativa.appendChild(botaoAlternativas);
+    }
+}
+mostraPergunta();
